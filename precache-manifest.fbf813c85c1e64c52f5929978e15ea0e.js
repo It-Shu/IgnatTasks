ab8de391fe74451576ad97a2a1e7ab311206a4d5
It-Shu <it.shustov@gmail.com>
2021-04-07 00:14:18 +0300

    Updates

---
+++ precache-manifest.fbf813c85c1e64c52f5929978e15ea0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c8806798d09810403ea69a696f93d5",
+    "url": "/IgnatTasks/index.html"
+  },
+  {
+    "revision": "a3e206b0b67cce629e77",
+    "url": "/IgnatTasks/static/css/main.fcf607f0.chunk.css"
+  },
+  {
+    "revision": "7c797cf766dfad3276e4",
+    "url": "/IgnatTasks/static/js/2.bda9ceaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IgnatTasks/static/js/2.bda9ceaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e206b0b67cce629e77",
+    "url": "/IgnatTasks/static/js/main.80b97805.chunk.js"
+  },
+  {
+    "revision": "7a556a60707f984e8cf7",
+    "url": "/IgnatTasks/static/js/runtime-main.775b33e3.js"
+  }
+]);
